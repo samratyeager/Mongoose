ead70f8f559b7e20b318da5046d90e660650fa42
Game <samratyeager@gmail.com>
2025-02-13 16:21:16 +0545

    learned and fixed the problem.

--- Mongodb Schema and Model/Models/Movies.js
+++ Mongodb Schema and Model/Models/Movies.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 
-//creating schema
-const movieSchema = new mongoose.Schema({
+const moviesSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    ratings: { type: Number, required: true, min: 1, max: 5 },
-    money: { type: mongoose.Decimal128, required: true, validate: v => v >= 10 },
-    genre: { type: Array },
-    isActive: { type:Boolean },
-    comments: [{ value: { type: String }, published: { type: Date, default: Date.now } }]
-});
+    ratings: {type: Number, required: true, min: 1, max: 5},
+    money: {
+        type: mongoose.Decimal128,
+        required: true,
+        validate: v => v >=10,
+    },
+    genre: {type: Array},
+    isActive: {type: Boolean},
+    comments: [{value:{type: String}, published: {type : Date, default: Date.now}}]
+})
 
-//creating model
-const movieModel = mongoose.model("Movie", movieSchema);
+
+const movieModel = mongoose.model("movie", moviesSchema);
+
 
 export default movieModel;
--- Mongodb Schema and Model/index.js
+++ Mongodb Schema and Model/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import connectDb from "./Connect DB/db.js";
 import movieModel from "./Models/Movies.js";
-import connectdbs from "./ConnectDb/db.js";
+import dotenv from "dotenv";  // Import dotenv
+
+dotenv.config();  // Load environment variables
+
 const app = express();
-const port = 8000;
-const Database_Url = process.env.Database_Url;
 
-connectdbs(Database_Url);
+const port = process.env.PORT
+const database_url = process.env.DATABASE_URL; 
 
-app.listen(port, () => console.log("Server up........"));
+connectDb(database_url);
 
+app.listen(port, () => console.log(`Server running on port ${port}...`));
